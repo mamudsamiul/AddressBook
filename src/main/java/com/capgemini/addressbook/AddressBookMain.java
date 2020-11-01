cad536aba0220e768e9b8f2ab941627aee6ee197
mamudsamiul <samiulmamud1995@gmail.com>
2020-11-01 20:42:15 +0530

    (Samiul Mamud) Created: AddressBook System Program is created

---
+++ src/main/java/com/capgemini/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.capgemini.addressbook;
+
+public class AddressBookMain {
+	public static void main(String args[]) {
+		System.out.println("Welcome to the addressBook Program");
+	}
+}
---
+++ src/test/java/com/capgemini/addressbooktest/AddressBookTest.java
@@ -0,0 +1,10 @@
+package com.capgemini.addressbooktest;
+
+import org.junit.Test;
+
+import com.capgemini.addressbook.AddressBookMain;
+
+import static org.junit.Assert.*;
+
+public class AddressBookTest {
+}
