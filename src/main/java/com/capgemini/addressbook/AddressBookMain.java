551e926813185816cfcf8417db7c11be40e43b5e
mamudsamiul <samiulmamud1995@gmail.com>
2020-11-01 20:51:38 +0530

    (Samiul Mamud) Added: Ability to create Contacts in AddressBook

--- src/main/java/com/capgemini/addressbook/AddressBookMain.java
+++ src/main/java/com/capgemini/addressbook/AddressBookMain.java
@@ -3,5 +3,6 @@ package com.capgemini.addressbook;
 public class AddressBookMain {
 	public static void main(String args[]) {
 		System.out.println("Welcome to the addressBook Program");
+		Contacts contact = new Contacts();
 	}
 }
--- src/test/java/com/capgemini/addressbooktest/AddressBookTest.java
+++ src/test/java/com/capgemini/addressbooktest/AddressBookTest.java
@@ -3,8 +3,59 @@ package com.capgemini.addressbooktest;
 import org.junit.Test;
 
 import com.capgemini.addressbook.AddressBookMain;
+import com.capgemini.addressbook.ValidateContact;
+
+import junit.framework.Assert;
 
 import static org.junit.Assert.*;
 
 public class AddressBookTest {
+	@Test
+	public void firstNameTest() {
+		ValidateContact contact = new ValidateContact();
+		boolean result = contact.validateFirstName("Samiul");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void lastNameTest() {
+		ValidateContact contact = new ValidateContact();
+		boolean result = contact.validateLastName("Mamud");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void emailTest() {
+		ValidateContact contact = new ValidateContact();
+		boolean result = contact.validateEmail("mamud.samiul@gmail.com");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void phoneNoTest() {
+		ValidateContact contact = new ValidateContact();
+		boolean result = contact.validatePhoneNo("91 1234567890");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void addressTest() {
+		ValidateContact contact = new ValidateContact();
+		boolean result = contact.validateAddress("majdia");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void cityTest() {
+		ValidateContact contact = new ValidateContact();
+		boolean result = contact.validateCity("berhampore");
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void stateTest() {
+		ValidateContact contact = new ValidateContact();
+		boolean result = contact.validateState("West Bengal");
+		Assert.assertTrue(true);
+	}
 }
